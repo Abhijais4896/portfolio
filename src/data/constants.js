645715ff49f4d6d45141604036b3a6eb95e85860
Abhishek Jaiswal <abhishek.77647@gmail.com>
2024-09-26 16:22:35 +0530

    files uploaded

---
+++ src/data/constants.js
@@ -0,0 +1,315 @@
+export const Bio = {
+  name: "Abhishek Jaiswal",
+  roles: [
+    "Software Developer",
+    "Full Stack Developer",
+    "Devops Enthusiast",
+    "Backend Developer",
+  ],
+  description:
+    "As a dynamic and motivated B.Tech student specializing in Computer Science and Engineering, I am deeply driven by my unwavering passion for harnessing the transformative potential of software development, full stack development, devops and cloud technologies to tackle multifaceted problems. Armed with a solid foundation in the Java programming language, I possess an extensive skill set and proficiency in utilizing a comprehensive stack of technical tools, including ReactJS, NextJs, NodeJS, SQL, MongoDb, Redis, Postegresql and devops tools like Docker, kubernetes, jenkins  and Terraform.",
+  github: "https://github.com/Abhijais4896",
+  resume:
+    "https://drive.google.com/file/d/18zwoh7qqNDZ8mgYJK9ys9o044PLEkKpY/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/abhishekjaiswal076/",
+  twitter: "https://twitter.com/Abhishek_4896",
+  insta: "https://www.instagram.com/abhishekjaiswal4896/",
+  facebook: "https://www.facebook.com/abhishek.jaishwal.3979",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://tse1.mm.bing.net/th?id=OIP.n8pa_ux7uUyU9CJrzb1scAHaHa&pid=Api&P=0&h=180",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://tse4.mm.bing.net/th?id=OIP.DN7ToydkJZEdVaJVK_NhvwHaHa&pid=Api&P=0&h=180",
+      },
+      {
+        name: "HTML",
+        image:
+          "https://tse2.mm.bing.net/th?id=OIP.744mxzC5wmuJwSAv6Ej3XwHaHa&pid=Api&P=0&h=180",
+      },
+      {
+        name: "CSS",
+        image: "https://tse1.mm.bing.net/th?id=OIP.9GLqOe56aIqcRvqi0jCQegHaE8&pid=Api&P=0&h=180",
+      },
+      {
+        name: "TypeScript",
+        image:
+          "https://tse2.mm.bing.net/th?id=OIP.maKe3jXsLd8flovNsX2_3QHaHa&pid=Api&P=0&h=180",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://tse2.mm.bing.net/th?id=OIP._jgRdIxBtUKFMCndkSCYIQAAAA&pid=Api&P=0&h=180",
+      },
+      {
+        name: "ReactJs",
+        image:
+          "https://tse3.mm.bing.net/th?id=OIP.zSpmMQqJdRpRKyD4-YpZGAHaEO&pid=Api&P=0&h=180",
+      },
+      {
+        name: "NextJs",
+        image:
+          "https://tse3.mm.bing.net/th?id=OIP.eXF6SW34jHHps4_SqD1FzgHaHa&pid=Api&P=0&h=180",
+      },
+      {
+        name: "Three.Js",
+        image:
+          "https://tse3.mm.bing.net/th?id=OIP.bAlUwmmvWIIvSxL5yyNmiwHaE7&pid=Api&P=0&h=180",
+      },
+      
+    ],
+  },
+  {
+    title: "Backend & Databases",
+    skills: [
+      {
+        name: "NodeJs",
+        image:
+        "https://tse1.mm.bing.net/th?id=OIP.ttBte4Hud6ToTEDCVT3Y1wHaFj&pid=Api&P=0&h=180",
+      },
+      {
+        name: "ExpreeJs",
+        image:
+        "https://tse2.mm.bing.net/th?id=OIP.1oZo0qgO2ybBNcjCWcx06QHaDP&pid=Api&P=0&h=180",
+      },
+      {
+        name: "MongoDb",
+        image: "https://tse2.mm.bing.net/th?id=OIP.ogXjw1C_vZgNjiT3myLFHAHaD4&pid=Api&P=0&h=180",
+      },
+      {
+        name: "PostgreSql",
+        image:
+          "https://tse2.mm.bing.net/th?id=OIP.CG8qTeTuoei796LdScT2bwHaIP&pid=Api&P=0&h=180",
+      },
+      {
+        name: "MySql",
+        image:
+          "https://tse4.mm.bing.net/th?id=OIP.PJ5Pne8V5OG3JcCvNgmgWQHaHa&pid=Api&P=0&h=180",
+      },
+      {
+        name: "Redis",
+        image: "https://tse3.mm.bing.net/th?id=OIP.xwmYwJcR7JzGs3PVQntPPwHaFH&pid=Api&P=0&h=180",
+      },
+      {
+        name: "WebSockets",
+        image:
+          "https://tse2.mm.bing.net/th?id=OIP.cAcjW9zw1GCKyX0894kR-QHaFc&pid=Api&P=0&h=180",
+      },
+      
+    ],
+  },
+  {
+    title: "DevOPs",
+    skills: [
+      {
+        name: "Shell Scripting",
+        image:
+          "https://tse3.mm.bing.net/th?id=OIP.OfzyptWloQmayFh5ttIjSwHaEH&pid=Api&P=0&h=180",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://tse2.mm.bing.net/th?id=OIP.dEgEQ0JBlwn323Q_i0spsgAAAA&pid=Api&P=0&h=180",
+      },
+      {
+        name: "Kubernetes",
+        image:
+          "https://tse4.mm.bing.net/th?id=OIP.XYF7bCFcLMU7-54vkFwXRwHaEK&pid=Api&P=0&h=180",
+      },
+      {
+        name: "Jenkins",
+        image:
+          "https://tse4.mm.bing.net/th?id=OIP.164u8ROsNLuAggpHo5QFBwHaFx&pid=Api&P=0&h=180",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://tse3.explicit.bing.net/th?id=OIP.Sd4Hxz8_CqID29kyj5-k9wHaEc&pid=Api&P=0&h=180",
+      },
+      {
+        name: "Ansible",
+        image:
+          "https://tse4.mm.bing.net/th?id=OIP.3Vz1SSdenkrK_LNsFsZwPwHaFj&pid=Api&P=0&h=180",
+      },
+      {
+        name: "Terraform",
+        image:
+          "https://tse4.mm.bing.net/th?id=OIP.6tash9tbpKnAAbhbBdn-fQHaIa&pid=Api&P=0&h=180",
+      },
+    ],
+  },
+  {
+    title: "Control Systems & Tools",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://tse2.mm.bing.net/th?id=OIP.9WZPLb2B4rKSbK52Zg8hnAHaEo&pid=Api&P=0&h=180",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Linux",
+        image:
+          "https://tse2.mm.bing.net/th?id=OIP.1Z-8lMw49LOJH4Vsrax8mgHaEK&pid=Api&P=0&h=180",
+      },
+      {
+        name: "WebRTC",
+        image:
+          "https://tse1.mm.bing.net/th?id=OIP.sBYXK8bFo4OHWWV3BKnYRgHaDt&pid=Api&P=0&h=180",
+      },
+      {
+        name: "Nginx",
+        image:
+          "https://tse1.mm.bing.net/th?id=OIP.pq0W4fIuX3SmE6H96jpsdwHaFj&pid=Api&P=0&h=180",
+      },
+      {
+        name: "Agile",
+        image:
+          "https://tse4.mm.bing.net/th?id=OIP.y5L1-bGNhPv1BAPQ21bsRwHaHa&pid=Api&P=0&h=180",
+      },
+      
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.innobyteservices.com/assets/images/transparentLogo.png",
+    role: "Backend Developer Intern",
+    company: "Innobyte Services",
+    date: "September 2024 - September 2024",
+    desc: "Designed and developed RESTful APIs for a blog application using Node.js and Express, enabling CRUD operations for posts, comments, and user managemennt. Optimized MongoDB queries, improving response time by 30% and ensuring scalability to handle large volumes of user-generated content."
+  ,
+    skills: [
+      "NodeJs",
+      "ExpressJs",
+      "MongoDb",
+      "Javascript",
+    ],
+  },
+  {
+    id: 0,
+    img: "https://img1.wsimg.com/isteam/ip/5d0888b8-f3bc-4e47-91c6-5e2d2262f57b/C__3_-removebg-preview.png/:/rs=w:90,h:90,cg:true,m/cr=w:90,h:90/qt=q:95",
+    role: "Devops Intern",
+    company: "Codtech IT Solutions",
+    date: "August 2024 - September 2024",
+    desc: "Set up and configured a fully automated CI/CD pipeline using Jenkins, streamlined the deployment process and reduced deployment time by 50% for a 3-tier web application. Integrated Jenkins with Kubernetes for seamless continuous delivery, implemented Container Orchestration to reduce downtime by 40%"
+,
+    skills: [
+      "Docker",
+      "Kubernetes",
+      "Jenkins",
+      "CI/CD pipeline",
+      
+    ],
+  },
+  {
+    id: 1,
+    img: "https://internship.aicte-india.org/uploads/logo/CORPORATE65117748884741695643464.png",
+    role: "Fullstack Developer Intern",
+    company: "Slash Mark",
+    date: "April 2024 - June 2024",
+    desc: "Developed a responsive social media platform using the MERN stack (MongoDB, Express.js, React.js, Node.js), increasing user engagement by 30% through real-time notifications, seamless navigation, and secure JWT-based authentication. Optimized API performance for handling posts, comments, and likes, reducing page load time by 40% with efficient database queries and caching strategies using Redis"
+,
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "Tailwind CSS",
+      "JavaScript",
+      "Redis",
+      "MongoDB",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://technocratsgroup.edu.in/wp-content/uploads/2024/02/TIT-excellence.jpg",
+    school: "Technocrats Institute Of Technology, Bhopal (Madhya Pradesh)",
+    date: "Oct 2021 - July 2025",
+    grade: "8.25 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Technocrats Institute of Technology(Excellence), Bhopal. I have completed 6 semesters and have a CGPA of 8.25. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://tse3.mm.bing.net/th?id=OIP.kWFlc-cjHs-1NMc-YNgOawAAAA&pid=Api&P=0&h=180",
+    school: "Eklavya Central Secondary School, Bihar",
+    date: "Apr 2019 - Apr 2020",
+    grade: "73%",
+    desc: "I completed my class 12 high school education at Eklavya Central School, Bihar.",
+    degree: "ISC(XII)",
+  },
+];
+
+export const projects = [
+  
+  {
+    id: 0,
+    title: "Saas Website Builder",
+    description:
+      "Developed a Full-Stack SaaS Website Builder using Next.js, TypeScript, Bun, and ShadcnUI, integrating features such as multi-vendor support, unlimited funnel hosting, and a comprehensive website and funnel builder. Implemented Advanced Payment Solutions with Stripe, including subscription plans, add-on products, Stripe Connect for user account management, and custom checkout processes, ensuring secure and efficient transactions. Created Role-Based Access and Custom Dashboards with detailed metrics and performance analytics, utilizing Prisma and MySQL for dynamic data management and media storage.",
+    image:
+      "https://storage.googleapis.com/msgsndr/0wtSXexlPhMN1945ooxW/media/65b7c3d24660477daf3d6708.png",
+    tags: ["NextJs Js", "MySql", "TypeScript", "NodeJs", "Prisma"],
+    category: "FullStack",
+    github: "https://github.com/Abhijais4896/SAAS-Website-Builder",
+    webapp: "#",
+    
+  },
+  {
+    id: 1,
+    title: "YouTube Summarization Extension",
+    description:
+      "Developed a Smart Chrome Extension with React, Tailwind CSS, and Plasmo, leveraging OpenAI to quickly summarize YouTube videos for streamlined content consumption.Enhanced User Navigation by integrating detailed transcriptions and timestamps, allowing users to efficiently find and review specific sections of videos",
+    image:
+      "https://tse4.mm.bing.net/th?id=OIP.v9qYhJ066j_M8naROome1QHaEo&pid=Api&P=0&h=180",
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "Redux",
+      "Plasmo",
+      "OpenAI",
+    ],
+    category: "FullStack",
+    github: "https://github.com/Abhijais4896/You-Tube-Summarization",
+    webapp: "#",
+  },
+  {
+    id: 2,
+    title: "Voice Chat Web App",
+    description:
+      "Built a Real-Time Voice Chat Application using the MERN stack (MongoDB, Express, React, Node.js), WebRTC, and Socket.io, enabling seamless voice communication and interactive features.Performed Unit Testing to ensure robust functionality and high performance, while integrating features like real-time messaging and voice chat.Dockerized the application to facilitate consistent and efficient deployment across various environments, enhancing scalability and reliability.",
+
+    image:
+      "https://images.unictool.com/unictoolen/assets/article/magicvox_article/magicvox-cartoon-character-ai-voice-generator-text-to-speech.jpg",
+    tags: ["ReactJs", "NodeJs", "ExpressJs", "MongoDb", "WebRTC", "Socket.io"],
+    category: "FullStack",
+    github: "https://github.com/Abhijais4896/Voice-Chat-App---Mern",
+    webapp: "#",
+    
+  },
+  
+];
+
+
